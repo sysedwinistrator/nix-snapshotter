d887dba4151ab356fb4ea18cc86acf7577730bdc
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2024-04-13 14:33:44 +0200

    use realistic test refs in image_service tests
    - add latest tag which should always be present in ref received by CRI
    - use real store paths built with nix-snapshotter.buildImage

--- pkg/nix/image_service_test.go
+++ pkg/nix/image_service_test.go
@@ -9,23 +9,23 @@ func TestGetNixStorePath(t *testing.T) {
 	ctx := context.Background()
 
 	// Single arch ref
-	ref := "nix:0/nix/store/02zg1wk37s9k35n5iv850g52dp1ffdxz-nginx-1.24.0"
-	expected := "/nix/store/02zg1wk37s9k35n5iv850g52dp1ffdxz-nginx-1.24.0"
+	ref := "nix:0/nix/store/dbc4mhv2fjbfx8pypx88qgp8nfp392az-nix-image-nginx.tar:latest"
+	expected := "/nix/store/dbc4mhv2fjbfx8pypx88qgp8nfp392az-nix-image-nginx.tar"
 	received := getNixStorePath(ctx, ref, "x86_64-linux")
 	if received != expected {
 		t.Fatalf("Expected %s, received %s", expected, received)
 	}
 
 	// Multi arch ref
-	ref = "nix:0/multiarch/x86_64-linux/nix/store/02zg1wk37s9k35n5iv850g52dp1ffdxz-nginx-1.24.0/aarch64-linux/nix/store/gjilixzvxk9pzilz3ixxamrjqk4mk1jl-nginx-1.24.0"
+	ref = "nix:0/multiarch/aarch64-linux/nix/store/zkw3cjabs8lc8bv4sgnm6x132gm956fc-nix-image-nginx.tar/x86_64-linux/nix/store/dbc4mhv2fjbfx8pypx88qgp8nfp392az-nix-image-nginx.tar:latest"
 
-	expected = "/nix/store/02zg1wk37s9k35n5iv850g52dp1ffdxz-nginx-1.24.0"
+	expected = "/nix/store/dbc4mhv2fjbfx8pypx88qgp8nfp392az-nix-image-nginx.tar"
 	received = getNixStorePath(ctx, ref, "x86_64-linux")
 	if received != expected {
 		t.Fatalf("Expected %s, received %s", expected, received)
 	}
 
-	expected = "/nix/store/gjilixzvxk9pzilz3ixxamrjqk4mk1jl-nginx-1.24.0"
+	expected = "/nix/store/zkw3cjabs8lc8bv4sgnm6x132gm956fc-nix-image-nginx.tar"
 	received = getNixStorePath(ctx, ref, "aarch64-linux")
 	if received != expected {
 		t.Fatalf("Expected %s, received %s", expected, received)
