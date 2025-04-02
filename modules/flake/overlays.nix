0fab6ad62c0074d5904ff1b3dec3c66561e81917
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2025-04-02 20:14:57 +0200

    update hash and patch for k3s 1.30.11

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -11,7 +11,8 @@
         vendorHash = {
           "sha256-fs9p6ywS5XCeJSF5ovDG40o+H4p4QmEJ0cvU5T9hwuA=" = "sha256-htanp0VOMadzoIyPUT8kOTSb58sz5DHlVBVGbY13ejU=";
           "sha256-q/cRKuqXuzPcLEYD+BH82ZAc+ZgGIqKWLsM1E4uQsok=" = "sha256-tJBi4b3OGrGbc49+LNx0PXwj9NuvBB55DNvIG1VCMh8=";
-          "sha256-HTUFv8WBIDiBQ860p3RiROF+kDzBekvgBAr2TJh036E=" =  "sha256-Wzd0ief4HnMc/EW+AZOabTdl2yYJWohn51Unw1Nj850=";
+          "sha256-HTUFv8WBIDiBQ860p3RiROF+kDzBekvgBAr2TJh036E=" = "sha256-Wzd0ief4HnMc/EW+AZOabTdl2yYJWohn51Unw1Nj850=";
+          "sha256-G7RUyFzg3B4X0tdKmD1ep9a4cnVkUmFqBP5t1s8uFLc=" = "sha256-nHlMqR3fkt+tLb4K0P5PWQ9Fj7J9vzy/01HfCdsJODY=";
         }.${args.vendorHash};
         # Source https://patch-diff.githubusercontent.com/raw/k3s-io/k3s/pull/9319.patch
         # Remove when merged
